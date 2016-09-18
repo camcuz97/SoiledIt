e20c08445c45e8dad0c667f2caaaa4848b392105
Cameron Cuzmanko <camcuz1@gmail.com>
2016-09-18 01:51:09 -0500

    Made file into a class

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -4,75 +4,80 @@ import csv
 # Columns
 # 0 = musym, 1 = mukey, 2 = lat, 3 = long, 4 = muname, 5 = wDepAnnMin, 6 = wDepAJMin, 7 = aws25, 8 = aws50, 9 = aws100, 10 = aws150, 11 = drdc, 12 = drw
 
-def printCol(csv):
-    for row in csv:
-        print row[0]
+class analysis:
 
-def findData(minX, maxX, minY, maxY):
-    return 0
+    def __init__(self):
+        with open('data/sortedDataTrunc.csv') as csvfile:
+            readCSV = csv.reader(csvfile, delimiter = ',')
+            i = 0
+            dict = {}
+            csvArray = []
+            for row in readCSV:
+                csvArray.append(row)
 
-def binSearch(csv, len, minX, maxX, minY, maxY):
-    mid = len/2
+    def printCol(csv):
+        for row in csv:
+            print row[0]
 
-def assignSoilQual(row):
-    return 0
+    def findData(minX, maxX, minY, maxY):
+        rowArray = []
+        for row in rowCSV:
+            if row[2] >= minX and row[2] <= maxX and row[3] >= minY and row[3] <= maxY:
+                rowArray.append(row)
+        return fitnessFunct(rowArray)
 
-def fitnessFunct(rowArray):
-    tupleArray = []
-    for row in rowArray:
-        tempScore = findFitness(row)
-        tupleArray.append((row[2],row[3],tempScore))
-    return tupleArray
+    def binSearch(csv, len, minX, maxX, minY, maxY):
+        mid = len/2
 
-def findFitness(row):
-    score = 0
-    #first checks drainage (most important)
-    if row[11] == 'Well drained':
-        score += 15
-    elif row[11] == 'Moderately well drained':
-        score += 8
-    elif row[11] == 'Somewhat poorly drained':
-        score += 4
-    elif row[11] == 'Poorly drained':
-        score += 2
-    elif row[11] == 'Very poorly drained':
-        score += 1
-    #next checks soil's ability to store water
-    if row[7] <= 3.2:
-        score += 4
-    elif row[7] <= 4.6:
-        score += 6
-    elif row[7] <= 5.3:
-        score += 3
-    elif row[7] <= 5.93:
-        score += 1
-    #lastly checks water depth minimum
-    if row[5] >= 120:
-        score += 2
-    elif row[5] >= 80:
-        score += 4
-    elif row[5] >= 60:
-        score += 6
-    elif row[5] >= 40:
-        score += 3
-    else:
-        score +=1
+    def fitnessFunct(rowArray):
+        tupleArray = []
+        for row in rowArray:
+            tempScore = findFitness(row)
+            tupleArray.append((row[2],row[3],tempScore))
+        return tupleArray
 
-    return float(score)/27.0
+    def findFitness(row):
+        score = 0
+        #first checks drainage (most important)
+        if row[11] == 'Well drained':
+            score += 15
+        elif row[11] == 'Moderately well drained':
+            score += 8
+        elif row[11] == 'Somewhat poorly drained':
+            score += 4
+        elif row[11] == 'Poorly drained':
+            score += 2
+        elif row[11] == 'Very poorly drained':
+            score += 1
+        #next checks soil's ability to store water
+        if row[7] <= 3.2:
+            score += 4
+        elif row[7] <= 4.6:
+            score += 6
+        elif row[7] <= 5.3:
+            score += 3
+        elif row[7] <= 5.93:
+            score += 1
+        #lastly checks water depth minimum
+        if row[5] >= 120:
+            score += 2
+        elif row[5] >= 80:
+            score += 4
+        elif row[5] >= 60:
+            score += 6
+        elif row[5] >= 40:
+            score += 3
+        else:
+            score +=1
 
-with open('data/sortedDataTrunc.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter = ',')
-    i = 0
-    dict = {}
-    csvArray = []
-    for row in readCSV:
-        csvArray.append(row)
+        return float(score)/27.0
 
-    fitnessFunct(csvArray)
+        #fitnessFunct(csvArray)
+        #print findData(40.26, 40.38, -87.94567, -88.1093)
+            
+        #num_rows = 26256
 
-    num_rows = 26256
-
-    #binSearch(readCSV,num_rows,0,0,0,0)
+        #binSearch(readCSV,num_rows,0,0,0,0)
 
     
 
